e8f715ad1a19450ed2f2e4bcf5aae304a26ae544
Ove Stokke <14012923+ovestokke@users.noreply.github.com>
2023-11-30 12:39:20 +0000

    update app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [url, setUrl] = useState('');
+  const [result, setResult] = useState(null);
+
+  const handleImageAnalysis = () => {
+    // Implement image analysis logic here
+  };
+
+  const handleImageGeneration = () => {
+    // Implement image generation logic here
+  };
+
+  return (
+    <div>
+      <h1>Image Analyzer and Generator</h1>
+      <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} placeholder="Enter image URL" />
+      <button onClick={handleImageAnalysis}>Analyze Image</button>
+      <button onClick={handleImageGeneration}>Generate Image</button>
+      {result && <div>{result}</div>}
+    </div>
+  );
 }
 
 export default App;
